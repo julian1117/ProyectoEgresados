968c322a050f8968a4ae02eb9693aed70ab4c66b
laura posada <laura posada@DESKTOP-F91MVUQ>
2016-10-21 18:59:52 -0500

    editar contacto

---
+++ egresado/egresado-logica/src/test/resources/sqltest/Prueba-EditarContacto.sql
@@ -0,0 +1,5 @@
+insert into egresados.t_sectorlaboral(ID_SECTORLABORAL,NOM_SECTORLABORAL) values (24,'otra cosa')
+insert into egresados.t_departamento(ID_DEPARTAMENTO,NOMBRE_DEPARTAMENTO) values (74,'Caldas')
+insert into egresados.t_ciudad(ID_CIUDAD,NOM_CIUDAD,ID_DEPARTAMENTO) values (55,'Manizales',74)
+insert into egresados.t_empresa(ID_EMPRESA,CIUDAD,DEPARTAMENTO,DIRECCION,FAX,NIT,PAIS,RAZON_SOCIAL,SITIO_WEB,TELEFONO,TIPO_EMPRESA,ID_CIUDAD,ID_SECTOR) values (98,'Ibague','Tolima','cra 13','732','12345','Colombia','no se','cualquiercosa@gmail.com','33333','Privada',55,24)
+insert into egresados.t_contacto(CARGO_EMPRESA,EMAIL,NOMBRE,TELEFONO,ID_EMPRESA) values ('Dir','nose@gmail.com','Jorge posada','321232322',98)
