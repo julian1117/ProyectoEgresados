782ec9b885a1b985482317927323c08aad884ba4
laura posada <laura posada@DESKTOP-F91MVUQ>
2016-10-21 19:11:29 -0500

    correcion eliminar contacto

--- egresado/egresado-logica/src/test/resources/sqltest/Prueba-EliminarContacto.sql
+++ egresado/egresado-logica/src/test/resources/sqltest/Prueba-EliminarContacto.sql
@@ -1,4 +1,4 @@
-delete form egresados.t_contacto where ID_EMPRESA=98
+delete from egresados.t_contacto where ID_EMPRESA=98
 delete from egresados.t_empresa where ID_EMPRESA=98
 delete from egresados.t_sectorlaboral where ID_SECTORLABORAL = 24
 delete from egresados.t_ciudad where ID_CIUDAD=55
